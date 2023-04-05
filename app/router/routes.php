2cb4dcceda5962754e4d7ec00b739a09ed3a03d1
Rogério Soares <rgrsoares@yahoo.com.br>
2023-04-05 09:49:42 -0300

    Sistema de rotas com PHP - Parâmetros das rotas dinâmicas

--- app/router/routes.php
+++ app/router/routes.php
@@ -4,5 +4,6 @@ return [
     '/' => 'Home@index',
     '/user/create' => 'User@create',
     '/user/[a-z0-9]+' => 'User@index',
-    '/show/[a-z\-]+/[a-z\-]+/[0-9]+' => 'Article@show'
+    '/show/[a-z\-]+/[a-z\-]+/[0-9]+' => 'Article@show',
+    '/user/[0-9]+/name/[a-z]+'=> 'User@show'
 ];
